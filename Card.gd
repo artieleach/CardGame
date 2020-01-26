2ed25343c3f32bd51f47a236399cb005ce333fff
Artie <artieleach@gmail.com>
2020-01-25 22:42:04 -0600

    broken broken broken broken broken broken broken broken broken broken broken broken broken broken broken broken broken broken

--- Card.gd
+++ Card.gd
@@ -2,6 +2,7 @@ extends Area2D
 
 signal clicked
 signal update_board_pos
+signal update_my_z
 
 signal create_neighbor
 
@@ -19,18 +20,25 @@ var last_pos = null
 
 var shadow_position = Vector2(0, 0)
 
+var possible_neighbors = [Vector2(0, -1), Vector2(1, 0), Vector2(0, 1), Vector2(-1, 0)]
+
 var symbol_colors = [Color(0.0, 0.0, 0.533), Color(0.0, .235, 0.0), Color(.533, .0, .0), Color(.251, .251, .251)]
 
 func _ready():
 	last_pos = grid_pos
-	z_index = grid_pos.y
+	emit_signal("update_my_z", self, grid_pos.y)
 	$card.animation = str(randi() % 4)
 	update_card()
 	sleeping = true
 
 func _process(delta):
+	if sleeping:
+		modulate.a = 0.5
+	else:
+		modulate.a = 1
 	if held:
 		z_index = 64
+		#emit_signal("update_my_z", self, 64)
 
 func _input_event(viewport, event, shape_idx):
 	if event is InputEventMouseButton:
@@ -70,19 +78,18 @@ func update_card():
 	$value.animation = str(val)
 	$value.modulate = symbol_colors[sym_num]
 	z_index = grid_pos.y
+	emit_signal("update_board_pos", self, grid_pos)
 
 func take_turn(card_positions):
 	# inital set up, finding neighbors
 	board_state = card_positions
 	neighbors = []
-	for row in [-1, 0, 1]:
-		for col in [-1, 0, 1]:
-			if row == 0 or col == 0:
-				if 0 <= grid_pos.x + row and grid_pos.x + row < 5 and 0 <= grid_pos.y + col and grid_pos.y + col < 5:
-					neighbors.append([Vector2(grid_pos.x + row, grid_pos.y + col), board_state[grid_pos.x + row][grid_pos.y + col]])
-	if neighbors:
-		neighbors.remove(neighbors.find([grid_pos, self]))
-	#do game logic
+	print('self:', self)
+	for poss_n in possible_neighbors:
+		if 0 <= grid_pos.x + poss_n.x and grid_pos.x + poss_n.x < 5 and 0 <= grid_pos.y + poss_n.y and grid_pos.y + poss_n.y < 5:
+			neighbors.append([Vector2(grid_pos.x + poss_n.x, grid_pos.y + poss_n.y), board_state[grid_pos.x + poss_n.x][grid_pos.y + poss_n.y]])
+	print('neighbors: ',neighbors)
+	#do game logic 
 	sleeping = false
 	if not sleeping:
 		if sym_num == 0:
@@ -90,20 +97,25 @@ func take_turn(card_positions):
 			var has_grown = false
 			for neighbor in neighbors:
 				if neighbor[1]:
-					if neighbor[1].sym_num == 1 or neighbor[1].sym_num == 2 and not has_switched:
-						var holder = [int(neighbor[1].val), int(neighbor[1].sym_num)]
-						switch_pos(neighbor[1], true)
-						neighbor[1].update_card()
-						neighbor[1].sleeping = true
+					if neighbor[1].sym_num in [1, 2] and not has_switched:
+						switch_pos(neighbor[1], true, null)
+						var old_grid_pos = grid_pos
+						grid_pos = neighbor[1].grid_pos
+						neighbor[1].grid_pos = old_grid_pos
 						if neighbor[1].sym_num == 1:
 							neighbor[1].sym_num = 2
 						elif neighbor[1].sym_num == 2:
 							neighbor[1].sym_num = 1
+						neighbor[1].update_card()
+						neighbor[1].z_index = -100
+						neighbor[1].sleeping = true
 						has_switched = true
-				elif not neighbor[1] and not has_grown and not has_switched:
-					emit_signal("create_neighbor", 0, 1, neighbor[0])
-					has_grown = true
-			if not has_grown or not has_switched:
+			if not has_switched:
+				for neighbor in neighbors:
+					if not neighbor[1] and not has_grown:
+						emit_signal("create_neighbor", 0, 1, neighbor[0])
+						has_grown = true
+			if not has_grown and not has_switched:
 				sym_num = 3
 		elif sym_num == 1:
 			var has_grown = false
@@ -111,6 +123,7 @@ func take_turn(card_positions):
 				if neighbor[1]:
 					if neighbor[1].sym_num == 1:
 							val += 1
+							print('here', neighbor)
 				elif not has_grown:
 					emit_signal("create_neighbor", 1, 1, neighbor[0])
 					has_grown = true
@@ -150,25 +163,25 @@ func take_turn(card_positions):
 			survive = met_needs
 		update_card()
 
-func switch_pos(other_card, is_active):
+func switch_pos(other_card, is_active, new_pos):
 	#other card is the card this card is switching with, is_active determines who is moving and who is being moved
 	var tween = $Tween
+	var target_pos = other_card.position
+	var my_pos = position
 	if is_active:
-		var target_pos = other_card.position
 		z_index = 64
-		other_card.switch_pos(self, false)
 		tween.interpolate_property(self, "position", position, Vector2(position.x, position.y - 8), 0.2, Tween.TRANS_EXPO, Tween.EASE_OUT)
 		tween.start()
 		yield(tween, "tween_completed")
-		tween.interpolate_property(self, "position", position, Vector2(target_pos.x, position.y), 0.4, Tween.TRANS_EXPO, Tween.EASE_IN_OUT)
+		tween.interpolate_property(self, "position", position, Vector2(target_pos.x, target_pos.y - 8), 0.4, Tween.TRANS_EXPO, Tween.EASE_IN_OUT)
 		tween.start()
 		yield(tween, "tween_completed")
+		other_card.switch_pos(self, false, my_pos)
 		tween.interpolate_property(self, "position", position, Vector2(target_pos.x, target_pos.y), 0.2, Tween.TRANS_EXPO, Tween.EASE_OUT)
 		tween.start()
 		drop()
-		update_card()
 	else:
-		tween.interpolate_property(self, "position", position, other_card.position, 0.6, Tween.TRANS_EXPO, Tween.EASE_IN_OUT)
+		tween.interpolate_property(self, "position", position, new_pos, 0.2, Tween.TRANS_EXPO, Tween.EASE_IN_OUT)
 		tween.start()
 		update_card()
 
--- Table.gd
+++ Table.gd
@@ -10,7 +10,7 @@ var held_object = null
 var card_positions = []
 var empty_board = []
 
-var arr_size = rtg(screen_size)
+var arr_size = Vector2(5, 5)
 var cur_pos = Vector2(0, 0)
 signal turn_end
 
@@ -24,13 +24,8 @@ func _ready():
 		for j in range(arr_size.y):
 			card_positions[i].append([])
 	empty_board = card_positions.duplicate(true)
-	for i in range(2):
-		for j in range(1):
-			if randf() < 5:
-				create_card(randi() % 4, randi() % 6 + 1, Vector2(i, j))
-	for i in range(6):
-		for j in range(6):
-			create_card(randi() % 4, randi() % 6 + 1, Vector2(i+10, j))
+	create_card(0, randi() % 6 + 1, Vector2(0, 0))
+	create_card(1, randi() % 6 + 1, Vector2(1, 0))
 
 func _process(delta):
 	if held_object:
@@ -38,13 +33,13 @@ func _process(delta):
 		held_object.shadow_position = Vector2(int((held_object.position.x + tile_size.x / 2) / tile_size.x) * tile_size.x, 
 											  int((held_object.position.y + tile_size.y / 2) / tile_size.y) * tile_size.y)
 		$shadow.position = held_object.shadow_position
-	elif Input.is_action_just_released("ui_accept"):
+	elif Input.is_action_just_released("ui_accept" ):
 		print(cur_pos,card_positions[cur_pos.x][cur_pos.y])
 		if card_positions[cur_pos.x][cur_pos.y]:
 			card_positions[cur_pos.x][cur_pos.y].take_turn(card_positions)
-		if cur_pos.x < 5:
+		if cur_pos.x < 4:
 			cur_pos.x += 1
-		elif cur_pos.y < 5:
+		elif cur_pos.y < 4:
 			cur_pos.x = 0
 			cur_pos.y += 1
 		else:
@@ -61,12 +56,16 @@ func create_card(sym_num: int, card_val: int, pos: Vector2):
 		new_card.connect("clicked", self, "_on_pickable_clicked")
 		new_card.connect("update_board_pos", self, "_on_update_board_pos")
 		new_card.connect("create_neighbor", self, "create_card")
-		self.connect("turn_end", new_card, "_on_end_turn")
+		new_card.connect("update_my_z", self, "_update_card_z")
+		connect("turn_end", new_card, "_on_end_turn")
 		new_card.grid_pos = pos
 		new_card.position = pos * tile_size  # do some research on making a bordered game space
 		card_positions[pos.x][pos.y] = new_card
 		add_child(new_card)
-	
+
+func _update_card_z(card, z):
+	card.z_index = z
+
 func _on_pickable_clicked(object):
 	if not held_object:
 		$shadow.visible = true
@@ -80,21 +79,13 @@ func _unhandled_input(event):
 	if event is InputEventMouseButton:
 		if event.button_index == BUTTON_LEFT:
 			if held_object and not event.pressed:
-				held_object.position = held_object.shadow_position
-				held_object.grid_pos  =Vector2(int(held_object.position.x / tile_size.x), int(held_object.position.y / tile_size.y))
-				var obj_pos = held_object.grid_pos
-				$shadow.visible = false
+				card_positions[held_object.grid_pos.x][held_object.grid_pos.y] = []
+				held_object.grid_pos = Vector2(int(held_object.position.x) / tile_size.x, int(held_object.position.y) / tile_size.y).round()
+				if card_positions[held_object.grid_pos.x][held_object.grid_pos.y]:
+					held_object.grid_pos = held_object.last_pos
+				held_object.position = held_object.grid_pos * tile_size
 				held_object.drop()
-				var old_pos = held_object.last_pos
-				if 0 <= obj_pos.y and obj_pos.y < len(card_positions[0]) and 0 <= obj_pos.x and obj_pos.x < len(card_positions) and not card_positions[obj_pos.x][obj_pos.y]:
-					card_positions[old_pos.x][old_pos.y] = []
-					card_positions[obj_pos.x][obj_pos.y] = held_object
-				else:
-					held_object.grid_pos = obj_pos
-				held_object.update_card()
 				held_object = null
 		elif event.button_index == BUTTON_RIGHT:
 			if event.pressed:
-				create_card(randi() % 4, randi() % 6 + 1, rtg(get_global_mouse_position()))
-
-
+				create_card(randi() % 4, randi() % 6 + 1, rtg(get_local_mouse_position()))
